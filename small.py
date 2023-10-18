37d78f7f1e623078a8b56c31d567ea53b26529bb
Anshika2102 <147030619+Anshika2102@users.noreply.github.com>
2023-10-18 10:37:00 +0530

    Create small.py

---
+++ small.py
@@ -0,0 +1,11 @@
+# Input two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Compare the two numbers
+if num1 < num2:
+    print("The first number is smaller than the second number.")
+elif num1 > num2:
+    print("The first number is greater than the second number.")
+else:
+    print("The two numbers are equal.")
